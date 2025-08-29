16976392a0973770b8fb3465d152ab4b29fb015d
Aaron <epiihrawp@gmail.com>
2025-08-28 20:20:17 -0500

    Week 1: Additional Words 3 for Producer

--- producers/basic_producer_aaron.py
+++ producers/basic_producer_aaron.py
@@ -61,7 +61,7 @@ def get_message_interval() -> int:
 # Define some lists for generating buzz messages
 ADJECTIVES: list = ["amazing", "too much", "boring", "not enough", "exciting", "weird"]
 ACTIONS: list = ["made", "lost", "gained", "raised", "spent" , "put", "wagered"]
-TOPICS: list = ["$50.00", "$1000.00", "$20000.00", "$130000.00", "$343.00", "$4000.00", "$53000.00", "$275000.00", "$1.50"]
+TOPICS: list = ["$50.00", "$1,000.00", "$20,000.00", "$130,000.00", "$343.00", "$4,000.00", "$53,000.00", "$275,000.00", "$1.50"]
 ACTIVITIES: list = ["Penny Stock", "Fantesy Football", "Sports Betting", "Stock Options", "Ethereum", "QQQ ETF"]
 TIME: list = ["yesterday", "an hour ago", "today", "last week", "last month"]
 
